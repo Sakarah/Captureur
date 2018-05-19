c8402d259e5510d003433e7cb267fc60794c0c35
Guillaume Bertholon <guillaume.bertholon@ens.fr>
2018-05-19 11:58:16 +0200

    First commit
    Add project files

---
+++ prologin.cc
@@ -0,0 +1,20 @@
+#include "prologin.hh"
+
+/// Fonction appelée au début de la partie.
+void partie_init()
+{
+    // Rien à initialiser
+}
+
+/// Fonction appelée à chaque tour.
+void jouer_tour()
+{
+    // TODO !
+}
+
+/// Fonction appelée à la fin de la partie.
+void partie_fin()
+{
+    // Rien à libérer
+}
+
